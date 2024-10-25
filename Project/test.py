08fba703263732d1de8a18bb6bdbe025d2e85044
ella-mzg <ella.mzgh@gmail.com>
2024-10-25 16:33:22 +0200

    update test file

--- Project/test.py
+++ Project/test.py
@@ -6,10 +6,12 @@ class TestUtils(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(add_numbers(5, 3), 8)
         self.assertEqual(add_numbers(-1, 1), 0)
+        print("Addition test passed")
 
     def test_subtract_numbers(self):
         self.assertEqual(subtract_numbers(10, 5), 5)
         self.assertEqual(subtract_numbers(0, 3), -3)
+        print("Subtraction test passed")
 
 if __name__ == '__main__':
     unittest.main()
